cc3bb6b16cd115a3adb5f39b48f83ed8a45ae70a
Rennan Macarini <rennangmacarini@gmail.com>
2023-03-29 15:51:57 -0300

    atualizando

--- src/Components/Filme/index.tsx
+++ src/Components/Filme/index.tsx
@@ -3,12 +3,8 @@ import styles from './styles.module.scss'
 export function Video() {
   
   return(
-    /*
     <> 
     <video className={styles.video} src="/video/mariano.mp4" muted autoPlay loop></video>
     </>
-    */
-   <>
-   </>
   )
 }
